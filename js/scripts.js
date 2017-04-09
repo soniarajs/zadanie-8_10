41acc53430da713b5dc648eb0c943975f3065f06
Sonia R <Sonia R>
2017-04-09 20:00:38 +0200

    new

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var buttonClass = document.getElementsByClassName('button');
+for (var k = 0 ; k=length ; k++) {
+  function showButton(buttonClass) {
+  	console.log(buttonClass.innerText);
+  }
+}
